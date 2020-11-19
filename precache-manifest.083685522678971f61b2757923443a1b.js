a8340dfc4a6174ce0e4df5ab0757e90b2ead87e0
Rajiv Rawat <rajivking25@gmail.com>
2020-11-19 14:52:46 +0530

    Updates

---
+++ precache-manifest.083685522678971f61b2757923443a1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48dea441909e4b65e2c6206b1494cbbf",
+    "url": "/trial/index.html"
+  },
+  {
+    "revision": "5f0854368e44197748e4",
+    "url": "/trial/static/css/main.81a7aac1.chunk.css"
+  },
+  {
+    "revision": "b5ff7303f3e2aad0d816",
+    "url": "/trial/static/js/2.10d3d82f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/trial/static/js/2.10d3d82f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f0854368e44197748e4",
+    "url": "/trial/static/js/main.308847ad.chunk.js"
+  },
+  {
+    "revision": "4e1e385e6d633af753ff",
+    "url": "/trial/static/js/runtime-main.e123e1ec.js"
+  }
+]);
